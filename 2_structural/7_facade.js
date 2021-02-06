cf046ab41820d7edb8818faa51c72e3758f2aaf6
Садаев Шынгыс <sadaevsh@gmail.com>
2021-02-06 17:33:26 +0600

    structur pattern faced

---
+++ 2_structural/7_facade.js
@@ -0,0 +1,41 @@
+class Complaints {
+    constructor() {
+        this.complaints = []
+    }
+    reply(complaint) {}
+    add(complaint) {
+        this.complaints.push(complaint)
+        return this.reply(complaint)
+    }
+}
+
+class ProductComplaints extends Complaints {
+    reply({id, customer, details}){
+        return `Product: ${id}: ${customer} (${details})`
+    }
+}
+
+class ServiceComplaints extends Complaints {
+    reply({id, customer, details}){
+        return `Service: ${id}: ${customer} (${details})`
+    }
+}
+
+class ComplaintRegistry {
+    register(customer, type, details) {
+        const id = Date.now()
+        let complaint
+        if(type === 'service') {
+            complaint = new ServiceComplaints()
+        }else{
+            complaint = new ProductComplaints()
+        }
+
+        return complaint.add({id, customer, details})
+    }
+}
+
+const registery = new ComplaintRegistry()
+
+console.log(registery.register('Vladilen', 'service', 'недоступен'))
+console.log(registery.register('Elena', 'product', 'недоступен'))
