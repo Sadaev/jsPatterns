db4a5a71a2206410046f398227507896bff65f7b
Садаев Шынгыс <sadaevsh@gmail.com>
2021-02-06 16:33:27 +0600

    Patttern crteater constructor

--- 1_creatinal/1_constructor.js
+++ 1_creatinal/1_constructor.js
@@ -1,4 +1,28 @@
-function Server(name, ip) {
-    this.name = name
-    this.ip = ip
+// function Server(name, ip) {
+//     this.name = name
+//     this.ip = ip
+// }
+
+// Server.prototype.getUrl = function() {return `https://${this.ip}:80`}
+
+// const aws = new Server('Aws German', '82.21.21.32')
+
+// console.log(aws.getUrl())
+
+class Server {
+    constructor(name, ip) {
+        this.name = name
+        this.ip = ip
+    }
+
+    getUrl() {
+        return `https://${this.ip}:80`
+    }
 }
+
+const aws = new Server('Aws German', '82.21.21.32')
+const aws_kax = new Server('Aws kazakhstan', '1111111111')
+
+console.log(aws.getUrl())
+console.log(aws_kax.getUrl())
+
